8adf372a53ff1f493c6ec83f4d3c5dc284973883
Zuko <karatov7@gmail.com>
2024-09-07 23:13:28 +0300

    feat: create the project

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<!-- src/App.vue -->
+<template>
+    <div id="app">
+      <main>
+        <router-view />
+      </main>
+    </div>
+  </template>
+  
+  <script setup>
+
+  </script>
+  
+  <style scoped>
+  h1 {
+    color: #42b983;
+  }
+  </style>
+  
---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<!-- src/views/Home.vue -->
+<template>
+    <div>
+      <!-- <h1>Welcome to the Home Page!</h1> -->
+    </div>
+  </template>
+  
+  <script setup>
+  </script>
+  
+  <style scoped>
+  h1 {
+    color: #42b983;
+  }
+  </style>
+  
